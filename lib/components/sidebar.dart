96b101fb1e5536ab99d16dd755fa51cc033943bf
mayday <maydaythecoder@gmail.com>
2025-05-28 19:56:01 -0400

    Enhance Sidebar component with SVG logo and navigation options
    - Added SVG logo asset to the Sidebar for improved branding and visual appeal.
    - Implemented multiple ListTile entries for navigation, including Home, New & Featured, Men, Women, Kids, Sale, Settings, and Help, enhancing user navigation experience.
    - Introduced a loading placeholder for the SVG logo to ensure smooth rendering during asset loading.
    
    These changes significantly improve the Sidebar's functionality and user interface, providing a more comprehensive navigation experience within the app.

--- lib/components/sidebar.dart
+++ lib/components/sidebar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Sidebar extends StatelessWidget {
   const Sidebar({super.key});
+  final String assetName = 'assets/images/logo.svg';
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +11,91 @@ class Sidebar extends StatelessWidget {
       child: Column(
         children: [
           SizedBox(height: MediaQuery.of(context).padding.top),
-          const Text('Sidebar'),
+          Align(
+            alignment: Alignment.centerRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: SvgPicture.asset(
+                assetName,
+                width: 50,
+                height: 50,
+                semanticsLabel: 'Nike Logo',
+                placeholderBuilder:
+                    (BuildContext context) => Container(
+                      padding: const EdgeInsets.all(2.0),
+                      child: const CircularProgressIndicator(),
+                    ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 24),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.home_outlined, size: 20),
+            title: const Text('Home', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.shopping_bag_outlined, size: 20),
+            title: const Text('New & Featured', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.man_outlined, size: 20),
+            title: const Text('Men', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.woman_outlined, size: 20),
+            title: const Text('Women', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.child_care_outlined, size: 20),
+            title: const Text('Kids', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.percent_outlined, size: 20),
+            title: const Text('Sale', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          const Spacer(),
+          const Divider(),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.settings_outlined, size: 20),
+            title: const Text('Settings', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            dense: true,
+            leading: const Icon(Icons.help_outline, size: 20),
+            title: const Text('Help', style: TextStyle(fontSize: 14)),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+          const SizedBox(height: 32),
         ],
       ),
     );
