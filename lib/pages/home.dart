a94285b374e1076c47d079c5a3ac137f9c7cda4c
mayday <maydaythecoder@gmail.com>
2025-05-28 15:44:51 -0400

    Enhance Flutter project with new assets and dependencies
    - Added SVG logo asset and multiple JPEG images to the assets/images directory.
    - Updated pubspec.yaml to include flutter_svg dependency (version ^2.0.9) for SVG support.
    - Modified pubspec.lock to reflect new dependencies and their versions.
    - Updated Home widget to utilize the new SVG logo with a placeholder during loading.
    
    This commit improves the visual assets of the application and ensures compatibility with SVG formats.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 class Home extends StatefulWidget {
   const Home({super.key, required this.title});
   final String title;
   @override State<Home> createState() => _HomeState();}
 
+
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    const String assetName = 'assets/images/logo.svg';
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -18,7 +19,14 @@ class _HomeState extends State<Home> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            SvgPicture.asset('assets/images/logo.svg'),
+            SvgPicture.asset(
+              assetName,
+              semanticsLabel: 'Nike Logo',
+              placeholderBuilder: (BuildContext context) => Container(
+                padding: const EdgeInsets.all(30.0),
+                child: const CircularProgressIndicator(),
+              ),
+            ),
           ],
         ),
       ),
